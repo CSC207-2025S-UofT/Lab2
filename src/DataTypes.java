45336f146c600cd0ae3ec24e056bae2d4e15cb90
Godoftitan <ljc20060401@gmail.com>
2025-05-15 17:56:58 -0500

    new

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -4,6 +4,7 @@ public class DataTypes {
     public static long sum(List<Integer> numbers) {
         int a = 0;
         int s = 0;
+        int b =0;
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
             s += x;
